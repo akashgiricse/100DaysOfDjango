58bd2763646df23f058640169722cf8dfdcd127a
akashgiricse <akashgiricse@gmail.com>
2017-12-18 21:59:31 +0530

    Defined url for post_detail

---
+++ BlogApp/mysite/blog/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+	url(r'^$', views.post_list, name = 'post_list'),
+	url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'\
+		r'(?P<post>[-\w]+)/$',
+		views.post_detail, name = 'post_detail'),
+
+]
