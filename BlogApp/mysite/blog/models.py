f26701e6ad1352044b88d160665a3418678e746c
akashgiricse <akashgiricse@gmail.com>
2017-12-19 23:19:21 +0530

    Fixed typo and placed PublishedManager class before Post class

--- BlogApp/mysite/blog/models.py
+++ BlogApp/mysite/blog/models.py
@@ -4,8 +4,13 @@ from __future__ import unicode_literals
 from django.db import models
 from django.utils import timezone 
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
 # Create your models here.
+class PublishedManager(models.Manager):
+	def get_queryset(self):
+		return super(PublishedManager,self).get_queryset()\
+		.filter(status='published')
 
 class Post(models.Model):
 	STATUS_CHOICES=(
@@ -30,13 +35,9 @@ class Post(models.Model):
 	published = PublishedManager() # our custom manager.
 
 	def get_absolute_url(self):
-		return reverse('blog:post_detial',
+		return reverse('blog:post_detail',
 			args=[self.publish.year,
 			self.publish.strftime('%m'),
 			self.publish.strftime('%d'),
 			self.slug])
 
-class PublishedManager(models.Manager):
-	def get_queryset(self):
-		return super(PublishedManager,self).get_queryset()\
-		.filter(status='published')
