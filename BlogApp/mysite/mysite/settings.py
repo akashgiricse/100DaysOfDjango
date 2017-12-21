ee93b5331c5d760b6568a52172bcc7048da588b2
akashgiricse <akashgiricse@gmail.com>
2017-12-21 10:21:48 +0530

    Added EMAIL host,host_user etc

--- BlogApp/mysite/mysite/settings.py
+++ BlogApp/mysite/mysite/settings.py
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Email
+EMAIL_HOST = 'smtp.gmail.com' 
+EMAIL_HOST_USER = 'username@gmail.com' 
+EMAIL_HOST_PASSWORD = 'your_password' 
+EMAIL_PORT = 587
+EMAIL_USE_TLS= True
