c6c72bed211391e566fe7be5dbf7b68248686ea8
akashgiricse <akashgiricse@gmail.com>
2017-12-22 07:28:29 +0530

    Defined view for sending email

--- BlogApp/mysite/blog/views.py
+++ BlogApp/mysite/blog/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from django.core.mail import send_email
 
 from .forms import EmailPostForm
 from .models import Post
@@ -42,6 +43,7 @@ def post_detail(request,year,month, day, post):
 def post_share(request, post_id):
 	# Retrieve post by id
 	post = get_object_or_404(Post, id=post_id, status = 'published')
+	sent = False
 
 	if request.method == 'POST':
 		# Form was submitted
@@ -50,7 +52,12 @@ def post_share(request, post_id):
 			# Form fiend passed validation
 			cd = form.cleaned_data
 			# ... send email
+			post_url = request.build_absolute_uri(page.get_absolute_url())
+			subject = '{} ({}) recommends you reading "{}"'.format(cd['name'], cd['emai'], post.title)
+			message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title, post_url, cd['name'], cd['comments'])
+			send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+			sent = True
 	else:
 		form = EmailPostForm()
-	return render(request, 'blog/post/share.html', {'post':post, 'form': form})
+	return render(request, 'blog/post/share.html', {'post':post, 'form': form, 'sent': sent})
 
