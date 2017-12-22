12a304b14c2d9db1bdc5ff6ab3d71f46d2ed4daa
akashgiricse <akashgiricse@gmail.com>
2017-12-22 07:28:04 +0530

    Added url for sending email

--- BlogApp/mysite/blog/urls.py
+++ BlogApp/mysite/blog/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
 	url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$',
         views.post_detail,
         name='post_detail'),
+	url(r'^(?P<post_id>\d+)/share/$', views.post_share, name = 'post_share')
 
 ]
