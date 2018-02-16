3951d1c2b09186b30ca3ef05d53bd0dfd48e69a1
akashgiricse <akashgiricse@gmail.com>
2018-02-16 22:52:31 +0530

    Add similar post list

--- BlogApp/mysite/blog/views.py
+++ BlogApp/mysite/blog/views.py
@@ -8,6 +8,8 @@ from django.core.mail import send_mail
 from .forms import EmailPostForm, CommentForm
 from .models import Post, Comment
 from taggit.models import Tag
+from django.db.models import Count
+
 # Create your views here.
 
 
@@ -64,7 +66,15 @@ def post_detail(request,year,month, day, post):
 	else:
 		comment_form = CommentForm()
 
-	return render(request, 'blog/post/detail.html', {'post':post, 'comments': comments, 'comment_form':comment_form,})
+	# Lists of similar posts
+	post_tags_ids = post.tags.values_list('id', flat=True)
+	similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+	similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
+	return render(request, 'blog/post/detail.html', {'post':post,
+													 'comments': comments,
+													 'comment_form':comment_form,
+													 'similar_posts':similar_posts})
 
 def post_share(request, post_id):
 	# Retrieve post by id
