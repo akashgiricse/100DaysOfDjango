feb32a5fe6500331fcdb91a9af97497bad691849
akashgiricse <akashgiricse@gmail.com>
2018-02-19 23:48:41 +0530

    Add RSS feed

--- BlogApp/mysite/blog/urls.py
+++ BlogApp/mysite/blog/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from . import views
+from .feeds import LatestPostsFeed
 
 urlpatterns = [
 
@@ -10,5 +11,6 @@ urlpatterns = [
         name='post_detail'),
     url(r'^(?P<post_id>\d+)/share/$', views.post_share, name='post_share'),
     url(r'^tag/(?P<tag_slug>[-\w]+)/$', views.post_list, name='post_list_by_tag'),
+    url(r'^feed/$', LatestPostsFeed(), name='post_feed'),
 
 ]
