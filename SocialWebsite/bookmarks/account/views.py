cd2016208de37168d6f2a50874560a68dc5f04a8
akashgiricse <akashgiricse@gmail.com>
2018-02-22 01:17:57 +0530

    Create Login view and template

--- SocialWebsite/bookmarks/account/views.py
+++ SocialWebsite/bookmarks/account/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
 # Create your views here.
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'],
+                                password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated Successfully!')
+            else:
+                return HttpResponse('Disabled Account')
+        else:
+            return HttpResponse('Invalid Login')
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
