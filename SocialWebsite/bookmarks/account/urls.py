284bea13b724a8a2849622227bc76addf5d27c00
akashgiricse <akashgiricse@gmail.com>
2018-02-25 09:44:31 +0530

    Password change and Password reset url

--- SocialWebsite/bookmarks/account/urls.py
+++ SocialWebsite/bookmarks/account/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from . import views
 from django.contrib.auth.views import logout, login, logout_then_login
 from django.contrib.auth.views import password_change, password_change_done
+from django.contrib.auth.views import password_reset, password_reset_done
+from django.contrib.auth.views import password_reset_confirm, password_reset_complete
 urlpatterns = [
     # post view
     # url(r'^login/$', views.user_login, name='login'),
@@ -13,6 +15,12 @@ urlpatterns = [
     # Change password url
     url(r'^password-change/$', password_change, name='password_change'),
     url(r'^password-change/done/$', password_change_done, name='password_change_done'),
+    # restore password url
+    url(r'^password-reset/$', password_reset, name='password_reset'),
+    url(r'^password-reset/done/$', password_reset_done, name='password_reset_done'),
+    url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$',
+        password_reset_confirm, name='password_reset_confirm'),
+    url(r'^password-reset/complete/$', password_reset_complete, name='password_reset_complete'),
 
 
 ]
--- SocialWebsite/bookmarks/bookmarks/settings.py
+++ SocialWebsite/bookmarks/bookmarks/settings.py
@@ -130,3 +130,5 @@ STATICFILES_DIRS = (
 LOGIN_REDIRECT_URL = reverse_lazy('dashboard')
 LOGIN_URL = reverse_lazy('login')
 LOGOUT_URL = reverse_lazy('logout')
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
