2ab45892e00fe6ab04f23b53940c46a082a3265f
akashgiricse <akashgiricse@gmail.com>
2018-02-26 23:55:07 +0530

    Add screenshot, modify base.html to include signup link and defined url

--- SocialWebsite/bookmarks/account/urls.py
+++ SocialWebsite/bookmarks/account/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$',
         password_reset_confirm, name='password_reset_confirm'),
     url(r'^password-reset/complete/$', password_reset_complete, name='password_reset_complete'),
+    url(r'^register/$', views.register, name='register')
 
 
 ]
