3cf4d5532b229273bdf5a4b1070dd969ba4ae5ce
akashgiricse <akashgiricse@gmail.com>
2018-02-27 23:46:33 +0530

    Create UserEdit and ProfileEdit form

--- SocialWebsite/bookmarks/account/forms.py
+++ SocialWebsite/bookmarks/account/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 class LoginForm(forms.Form):
@@ -20,3 +21,15 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Password don\'t match.')
         return cd['password2']
+
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('date_of_birth', 'photo')
