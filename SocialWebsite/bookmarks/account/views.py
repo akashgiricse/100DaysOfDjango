1381bca18bfc9d102ecb2984398dc8792568a961
akashgiricse <akashgiricse@gmail.com>
2018-02-28 23:19:21 +0530

    Add custom email authentication

--- SocialWebsite/bookmarks/account/views.py
+++ SocialWebsite/bookmarks/account/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import authenticate, login
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
 from .models import Profile
-
+from django.contrib import messages
 # Create your views here.
 
 
@@ -67,6 +67,10 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated Successfully')
+
+        else:
+            messages.error(request, 'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
--- SocialWebsite/bookmarks/bookmarks/settings.py
+++ SocialWebsite/bookmarks/bookmarks/settings.py
@@ -135,3 +135,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTHENTICATION_BACKENDS = (
+
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+)
