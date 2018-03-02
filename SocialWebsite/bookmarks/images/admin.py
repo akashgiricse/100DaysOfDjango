286ecce55be7d874540ff440e9499a3ba27102e9
akashgiricse <akashgiricse@gmail.com>
2018-03-02 22:36:29 +0530

    Register Image in admin and app setting. Create users_like ManyToMany relationship

--- SocialWebsite/bookmarks/images/admin.py
+++ SocialWebsite/bookmarks/images/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Image
 # Register your models here.
+
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'image', 'created']
+    list_filter = ['created']
+
+
+admin.site.register(Image, ImageAdmin)
--- SocialWebsite/bookmarks/images/models.py
+++ SocialWebsite/bookmarks/images/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.utils.text import slugify
 # Create your models here.
 
 
@@ -15,5 +16,14 @@ class Image(models.Model):
     created = models.DateField(auto_now_add=True,
                                db_index=True)
 
+    users_like = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                        related_name='images_liked',
+                                        blank=True)
+
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+            super(Image, self).save(*args, **kwargs)
