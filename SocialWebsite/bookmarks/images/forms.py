204c0264e7263f26ca1ec8796273c8c5678af067
akashgiricse <akashgiricse@gmail.com>
2018-03-03 23:21:35 +0530

    Add form for saving image file

--- SocialWebsite/bookmarks/images/forms.py
+++ SocialWebsite/bookmarks/images/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import Image
+from urllib import request
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
 
 
 class ImageCreateForm(forms.ModelForm):
@@ -9,3 +12,27 @@ class ImageCreateForm(forms.ModelForm):
         widgets = {
             'url': forms.HiddenInput,
         }
+
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        valid_extentions = ['jpg', 'jpeg']
+        extention = url.rsplit('.', 1)[1].lower()
+        if extention is not valid_extentions:
+            raise forms.ValidationErro('The given URL does not match'
+                                       'valid image extention.')
+        return url
+
+    def save(self, force_insert=False,
+             force_update=False,
+             commit=True):
+        image = super(ImageCreateForm, self).save(commit=False)
+        image_url = self.cleaned_data['url']
+        image_name = '{}.{}'.format(slugify(image.title),
+                                    image_url.rsplit('.', 1)[1].lower())
+        # Download image from the give url
+        response = request.urlopen(image_url)
+        image.image.save(image_name, ContentFile(response.read()), save=False)
+
+        if commit:
+            image.save()
+        return image
