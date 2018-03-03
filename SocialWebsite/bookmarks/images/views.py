11636273b20579fce3c021ae34f1315542e06712
akashgiricse <akashgiricse@gmail.com>
2018-03-03 23:55:24 +0530

    Define views for create_image

--- SocialWebsite/bookmarks/images/views.py
+++ SocialWebsite/bookmarks/images/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import ImageCreateForm
 # Create your views here.
+
+
+@login_required
+def image_create(request):
+    """
+    View for creating an Image using the JavaScript Bookmarklet.
+    """
+    if request.method == 'POST':
+        # form is sent
+        form = ImageCreateForm(data=request.POST)
+        if form.is_valid():
+            # form data is valid
+            cd = form.cleaned_data
+            new_item = form.save(commit=False)
+            # assign current user to the item
+            new_item.user = request.user
+            new_item.save()
+            messages.success(request, 'Image added successfully')
+            # redirect to new created item detail view
+            return redirect(new_item.get_absolute_url())
+    else:
+        # build form with data provided by the bookmarklet via GET
+        form = ImageCreateForm(data=request.GET)
+
+    return render(request, 'images/image/create.html', {'section': 'images',
+                                                        'form': form})
